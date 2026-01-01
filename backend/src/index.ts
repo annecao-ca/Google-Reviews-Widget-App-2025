077fbe62acb4c2d66417fc415c4d7a6297e0dc87
Queenie Cao <queeniecao@Queenie-Cao.local>
2025-12-31 22:55:43 -0700

    fix dotenv import for production

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import dotenv from "dotenv";
+import * as dotenv from "dotenv";
 import path from "path";
 import { ReviewSyncService } from "./services/reviewSyncService";
 import { scheduleGoogleReviewSync } from "./jobs/syncGoogleReviews";
